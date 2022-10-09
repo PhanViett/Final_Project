aa137b5669ce03e01a37c05e0e87ef35e14f564a
PhanViett <72290215+PhanViett@users.noreply.github.com>
2022-10-09 21:17:43 +0700

    update

--- Frontend - ReactJs/src/routing/RootRouteObject.js
+++ Frontend - ReactJs/src/routing/RootRouteObject.js
@@ -31,7 +31,7 @@ let routers = [
     children: [
       {
         path: "quan-ly-nguoi-dung",
-        breadcrumb: "Quản lý người dùng",
+        breadcrumb: "Quản lý người dùngg",
         children: [
           {
             path: "",
