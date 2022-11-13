c6f81755f333576be577b20160484b5e88deb6b2
PhanViett <72290215+PhanViett@users.noreply.github.com>
2022-11-13 15:57:16 +0700

    update

--- Frontend1/src/app/modules/apps/user/Diagnostic.js
+++ Frontend1/src/app/modules/apps/user/Diagnostic.js
@@ -1,43 +1,175 @@
+import { useState } from "react";
+import { Form } from "react-bootstrap";
+
 export function Diagnostic () {
 
+
+    const [isUpdate, setIsUpdate] = useState(false);
 
     return (
         <div className="py-3">
             <div className="card-box" style={{ padding: "32px" }}>
                 <div className="row">
-                    <div className="col-12 mb-4">
-                        <h4 className="fw-bold">CHẨN ĐOÁN</h4>
+                    <div className="col-12 mb-8">
+                        <h4 className="fw-bold">CHẨN ĐOÁN TÌNH TRẠNG</h4>
                     </div>
                 
-                    <div className="col-9 mt-4 mb-10">
+                    <div className="col-9 mt-8">
                         <h5 className="fw-bold">Thông tin hiện tại</h5>
+                    </div>
+
+                    <div className="col-3 mb-10 text-end">
+                        {isUpdate === false ? 
+                            <button className="btn btn-primary py-0" style={{ fontSize: "13px", height: "38px" }} onClick={() => setIsUpdate(true)}>
+                                <i className="fas fa-plus me-1" style={{ fontSize: "12px" }}></i>Cập nhật
+                            </button>
+                        :
+                            <button className="btn btn-primary py-0" style={{ fontSize: "13px", height: "38px" }} onClick={() => setIsUpdate(false)}>
+                                <i className="fas fa-plus me-1" style={{ fontSize: "12px" }}></i>Lưu
+                            </button>
+                        }
 
                     </div>
 
-                    <div className="col-3 mt-4 mb-10 text-end">
-                        <button className="btn btn-primary py-0" style={{ fontSize: "13px", height: "38px" }}>
-                            <i className="fas fa-plus me-1" style={{ fontSize: "12px" }}></i>Cập nhật
-                        </button>
+
+                    <div className="row mx-1 mb-4">
+                        <div className="col-3">
+                            <span className="d-block mb-1" style={{fontWeight: 500}}>Họ tên: </span>
+                            {isUpdate === false ? 
+                                <span>Phan Văn Việt</span>
+                            :
+                                <Form.Control
+                                    type="text"
+                                    value="Phan Văn Việt"
+                                />
+                            }
+                        </div>
+                        <div className="col-3">
+                            <span className="d-block mb-1" style={{ fontWeight: 500 }}>Tuổi: </span>
+                            {isUpdate === false ?
+                                <span>Phan Văn Việt</span>
+                                :
+                                <Form.Control
+                                    type="text"
+                                    value="Phan Văn Việt"
+                                />
+                            }
+                        </div>
+                        <div className="col-3">
+                            <span className="d-block mb-1" style={{ fontWeight: 500 }}>Giới tính: </span>
+                            {isUpdate === false ?
+                                <span>Phan Văn Việt</span>
+                                :
+                                <Form.Control
+                                    type="text"
+                                    value="Phan Văn Việt"
+                                />
+                            }
+                        </div>
+                        <div className="col-3">
+                            <span className="d-block mb-1" style={{ fontWeight: 500 }}>Chiều cao: </span>
+                            {isUpdate === false ?
+                                <span>Phan Văn Việt</span>
+                                :
+                                <Form.Control
+                                    type="text"
+                                    value="Phan Văn Việt"
+                                />
+                            }
+                        </div>
+                    </div>                    
+                    <div className="row mx-1 mb-4">
+                        <div className="col-3">
+                            <span className="d-block mb-1" style={{ fontWeight: 500 }}>Cân nặng: </span>
+                            {isUpdate === false ?
+                                <span>Phan Văn Việt</span>
+                                :
+                                <Form.Control
+                                    type="text"
+                                    value="Phan Văn Việt"
+                                />
+                            }
+                        </div>
+                        <div className="col-3">
+                            <span className="d-block mb-1" style={{ fontWeight: 500 }}>Chiều cao: </span>
+                            {isUpdate === false ?
+                                <span>Phan Văn Việt</span>
+                                :
+                                <Form.Control
+                                    type="text"
+                                    value="Phan Văn Việt"
+                                />
+                            }
+                        </div>
+                        <div className="col-3">
+                            <span className="d-block mb-1" style={{ fontWeight: 500 }}>Cân nặng: </span>
+                            {isUpdate === false ?
+                                <span>Phan Văn Việt</span>
+                                :
+                                <Form.Control
+                                    type="text"
+                                    value="Phan Văn Việt"
+                                />
+                            }
+                        </div>
+                        <div className="col-3">
+                            <span className="d-block mb-1" style={{ fontWeight: 500 }}>Cholesterol: </span>
+                            {isUpdate === false ?
+                                <span>Phan Văn Việt</span>
+                                :
+                                <Form.Control
+                                    type="text"
+                                    value="Phan Văn Việt"
+                                />
+                            }
+                        </div>
                     </div>
-
-
-                    <div className="row">
-                        <div className="col-4">
-                            <span style={{fontWeight: 500}}>Họ tên: </span>
-                            <span>Phan Văn Việt</span>
+                    <div className="row mx-1 mb-4">
+                        <div className="col-3">
+                            <span className="d-block mb-1" style={{ fontWeight: 500 }}>Glucose: </span>
+                            {isUpdate === false ?
+                                <span>Phan Văn Việt</span>
+                                :
+                                <Form.Control
+                                    type="text"
+                                    value="Phan Văn Việt"
+                                />
+                            }
                         </div>
-                        <div className="col-4">
-                            <span style={{ fontWeight: 500 }}>Họ tên: </span>
-                            <span>Phan Văn Việt</span>
+                        <div className="col-3">
+                            <span className="d-block mb-1" style={{ fontWeight: 500 }}>Smoke: </span>
+                            {isUpdate === false ?
+                                <span>Phan Văn Việt</span>
+                                :
+                                <Form.Control
+                                    type="text"
+                                    value="Phan Văn Việt"
+                                />
+                            }
                         </div>
-                        <div className="col-4">
-                            <span style={{ fontWeight: 500 }}>Họ tên: </span>
-                            <span>Phan Văn Việt</span>
+                        <div className="col-3">
+                            <span className="d-block mb-1" style={{ fontWeight: 500 }}>Alcohol: </span>
+                            {isUpdate === false ?
+                                <span>Phan Văn Việt</span>
+                                :
+                                <Form.Control
+                                    type="text"
+                                    value="Phan Văn Việt"
+                                />
+                            }
+                        </div>
+                        <div className="col-3">
+                            <span className="d-block mb-1" style={{ fontWeight: 500 }}>Activity: </span>
+                            {isUpdate === false ?
+                                <span>Phan Văn Việt</span>
+                                :
+                                <Form.Control
+                                    type="text"
+                                    value="Phan Văn Việt"
+                                />
+                            }
                         </div>
                     </div>
-
-
-
                 </div>
             </div>
         </div>
--- Frontend1/src/app/modules/apps/user/Homepage.js
+++ Frontend1/src/app/modules/apps/user/Homepage.js
@@ -2,8 +2,40 @@ export function Homepage() {
 
 
     return (
-        <div>
-            
+        <div className="row">
+            <div className="col-12 ps-8 mt-4 mb-8">
+                <span style={{fontSize: "25px", fontWeight: 600}}>Dashboard</span>
+            </div>
+
+            <div className="col-3 px-9">
+                <div className="text-end px-6 pt-7 pb-10" style={{ backgroundColor: "green" }}>
+                    <span className="text-white">Số người dùng</span>
+                </div>
+            </div>
+
+            <div className="col-3 px-9">
+                <div className="text-end px-6 pt-7 pb-10" style={{ backgroundColor: "red" }}>
+                    <span className="text-white">Số tin tức</span>
+                </div>
+            </div>
+
+            <div className="col-3 px-9">
+                <div className="text-end px-6 pt-7 pb-10" style={{ backgroundColor: "violet" }}>
+                    <span className="text-white">Số chẩn đoán</span>
+                </div>
+            </div>
+
+            <div className="col-3 px-9">
+                <div className="text-end px-6 pt-7 pb-10" style={{ backgroundColor: "yellow" }}>
+                    <span className="text-white">Tỷ lệ chẩn đoán tốt</span>
+                </div>
+            </div>
+
+            <div className="col-12 ps-8 mt-8 mb-8">
+                <span style={{ fontSize: "25px", fontWeight: 600 }}>Tin tức</span>
+            </div>
+
+
         </div>
     )
 }
